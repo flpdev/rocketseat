2ccfa783314c9b75be9a20f535f2919010126d73
Felipe Moreira <fm.felipe@gmail.com>
2020-04-29 13:44:13 -0400

    29-04-2020

--- ReactJS/huntweb/src/pages/main/index.js
+++ ReactJS/huntweb/src/pages/main/index.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 import api from '../../services/api';
+import './style.css';
 
 export default class Main extends Component {
+
+    state = {
+        products: [],
+    }
 
     componentDidMount(){
         this.loadProducts();
@@ -9,12 +14,26 @@ export default class Main extends Component {
 
     loadProducts = async () => {
         const response = await api.get('/products');
+
+        this.setState({products : response.data.docs });
         
     }
 
-
-
     render(){
-        return <h1>Hello World!</h1>;
+
+        const {products} = this.state;
+
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+
+                        <a href="">Acessar</a>
+                    </article>
+                )) }
+            </div>
+        )
     }
 }
